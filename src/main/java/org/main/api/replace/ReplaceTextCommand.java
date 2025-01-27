b72cefda5609af1e1033ed8b7fa431544744777e
chripre <chripre@stud.ntnu.no>
2025-01-27 13:51:12 +0100

    feat: Small tweaks for replace classes -Tweaked classes and added get methods

--- src/main/java/org/main/api/replace/ReplaceTextCommand.java
+++ src/main/java/org/main/api/replace/ReplaceTextCommand.java
@@ -1,10 +1,15 @@
-package org.main.api.back_end;
+package org.main.api.replace;
+
+import static org.main.api.exceptions.ExceptionHandler.nullPointerException;
+import static org.main.api.exceptions.ExceptionHandler.numberInvalidException;
 
 public class ReplaceTextCommand {
   private final String target;
   private final String replacement;
 
   public ReplaceTextCommand(String target, String replacement) {
+    nullPointerException(target, replacement);
+    numberInvalidException(target, replacement);
     this.target = target;
     this.replacement = replacement;
   }
--- src/main/java/org/main/api/ui/TextCommand.java
+++ src/main/java/org/main/api/ui/TextCommand.java
@@ -1,13 +1,11 @@
 package org.main.api.ui;
 
-import org.main.api.back_end.ReplaceTextCommand;
-
 public class TextCommand {
   private String target;
   private String replacement;
-  ReplaceTextCommand replaceTextCommand = new ReplaceTextCommand(target, replacement);
+
 
   String execute(String text){
-    return replaceTextCommand.execute(text);
+    return null;
   }
 }
